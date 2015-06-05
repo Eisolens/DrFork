f67de6196e5d4e073858ede73a7590fa3bb84093
Sergey Pavliv <s.pavliv@samsung.com>
2015-06-05 13:48:40 +0300

    Add base tablet

--- Source/DrFork/DrForkBlockGrid.cpp
+++ Source/DrFork/DrForkBlockGrid.cpp
@@ -33,23 +33,23 @@ void ADrForkBlockGrid::BeginPlay()
 	const int32 NumBlocks = Size * Size;
 
 	// Loop to spawn each block
-	for(int32 BlockIndex=0; BlockIndex<NumBlocks; BlockIndex++)
-	{
-		const float XOffset = (BlockIndex/Size) * BlockSpacing; // Divide by dimension
-		const float YOffset = (BlockIndex%Size) * BlockSpacing; // Modulo gives remainder
+	//for(int32 BlockIndex=0; BlockIndex<NumBlocks; BlockIndex++)
+	//{
+	//	const float XOffset = (BlockIndex/Size) * BlockSpacing; // Divide by dimension
+	//	const float YOffset = (BlockIndex%Size) * BlockSpacing; // Modulo gives remainder
 
-		// Make postion vector, offset from Grid location
-		const FVector BlockLocation = FVector(XOffset, YOffset, 0.f) + GetActorLocation();
+	//	// Make postion vector, offset from Grid location
+	//	const FVector BlockLocation = FVector(XOffset, YOffset, 0.f) + GetActorLocation();
 
-		// Spawn a block
-		ADrForkBlock* NewBlock = GetWorld()->SpawnActor<ADrForkBlock>(BlockLocation, FRotator(0,0,0));
+	//	// Spawn a block
+	//	ADrForkBlock* NewBlock = GetWorld()->SpawnActor<ADrForkBlock>(BlockLocation, FRotator(0,0,0));
 
-		// Tell the block about its owner
-		if(NewBlock != NULL)
-		{
-			NewBlock->OwningGrid = this;
-		}
-	}
+	//	// Tell the block about its owner
+	//	if(NewBlock != NULL)
+	//	{
+	//		NewBlock->OwningGrid = this;
+	//	}
+	//}
 }
 
 
